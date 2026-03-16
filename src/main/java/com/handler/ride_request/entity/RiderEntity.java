6fecbb814a9a0351c0fb87cdc2d08789a6efce4e
hendafarhani <farhanii.henda@gmail.com>
2026-03-16 06:49:01 +0100

    chore: refactor RiderEntity to use Lombok annotations and add ID generation

--- src/main/java/com/handler/ride_request/entity/RiderEntity.java
+++ src/main/java/com/handler/ride_request/entity/RiderEntity.java
@@ -2,16 +2,23 @@ package com.handler.ride_request.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.*;
 
-@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "RIDER")
 public class RiderEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
