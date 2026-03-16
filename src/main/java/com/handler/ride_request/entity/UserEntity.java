547df2c1716774f9af08ce512b558b68e07e9833
hendafarhani <farhanii.henda@gmail.com>
2026-03-16 06:49:41 +0100

    chore: refactor UserEntity to use Lombok annotations and add ID generation

--- src/main/java/com/handler/ride_request/entity/UserEntity.java
+++ src/main/java/com/handler/ride_request/entity/UserEntity.java
@@ -2,18 +2,23 @@ package com.handler.ride_request.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 @Builder
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "USER")
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
