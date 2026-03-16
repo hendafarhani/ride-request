7da4d852c317a0c1509d203d1beca7bddeb130a2
hendafarhani <farhanii.henda@gmail.com>
2026-03-16 06:48:04 +0100

    chore: enable Kafka support in KafkaListenerConfiguration

--- src/main/java/com/handler/ride_request/kafka/configuration/KafkaListenerConfiguration.java
+++ src/main/java/com/handler/ride_request/kafka/configuration/KafkaListenerConfiguration.java
@@ -6,18 +6,18 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@ComponentScan(basePackages = {"com.example"})
+@EnableKafka
 @Configuration
 public class KafkaListenerConfiguration {
 
@@ -62,5 +62,4 @@ public class KafkaListenerConfiguration {
 
 
 }
-
 
