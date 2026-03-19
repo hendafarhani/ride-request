1652a96dbc9093ef7b3c26c9787d53bf26f13fb3
hendafarhani <farhanii.henda@gmail.com>
2026-03-20 00:38:14 +0100

    chore: update message converter in RabbitMQ configuration

--- src/main/java/com/handler/ride_request/rabbitmq/configuration/RabbitMQConfiguration.java
+++ src/main/java/com/handler/ride_request/rabbitmq/configuration/RabbitMQConfiguration.java
@@ -8,7 +8,8 @@ import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -49,7 +50,7 @@ public class RabbitMQConfiguration {
     }
 
     @Bean
-    public Jackson2JsonMessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+    public MessageConverter jsonMessageConverter() {
+        return new JacksonJsonMessageConverter();
     }
 }
